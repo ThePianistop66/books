37799aafe56e858668c336ec4b14bd6cf23ebbf7
ThePianistop66 <kassis.andrew@gmail.com>
2017-09-01 10:20:07 -0400

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript, the good parts', pages: 101},
+    {title: 'Harry potter', pages: 103},
+    {title: 'the dark tower', pages: 85},
+    {title: 'the hobbit', pages: 1}
+  ]
+}
